dbf32ff0fd80ed7fcd1fe0e5cb0fd3618c957515
THPhu2k <tranhoangphu10022006@gmail.com>
2022-12-17 18:16:28 +0700

    First Commite

---
+++ bt buoi 2/index.js
@@ -0,0 +1,10 @@
+// input arrays
+const arr1 = [1,2,4,5,6]
+const arr2 = [1,6,8,9,0]
+
+function isNotInArr1(elt) {
+  return arr1.includes(elt)
+}
+
+// output array
+console.log(arr2.filter(isNotInArr1))
